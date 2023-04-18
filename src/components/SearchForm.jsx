34db6b6b3b4fd0c95d400ea6aeac7a9f90ef96c2
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-18 14:40:43 -0400

    setup search form and initial function testing

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,6 +1,28 @@
 import { useGlobalContext } from '../contexts/context';
 
 const SearchForm = () => {
-  return <h2>Greeting</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchTerm = e.target.elements.search.value;
+    if (!searchTerm) return;
+  };
+
+  return (
+    <section>
+      <h1 className='title'>Search Images</h1>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='search'
+          placeholder='guitar'
+          className='form-input search-input'
+          // onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 export default SearchForm;
