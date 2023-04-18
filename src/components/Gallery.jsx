84d36d3a3fee7b838e663b2c2b99855f8f084da5
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-18 17:44:14 -0400

    setup image query and initial rendering

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,37 @@
+import { useQuery } from '@tanstack/react-query';
+import { useGlobalContext } from '../contexts/context';
+import axios from 'axios';
+
+const url =
+  'https://api.unsplash.com/search/photos?client_id=FF2DB1EgnIQtHGcajvAknkrTTbu5jvzriVAWFDNCMBs&query=office';
+
 const Gallery = () => {
-  return <h2>Gallery</h2>;
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url);
+
+      return result;
+    },
+  });
+
+  console.log(data);
+  if (isLoading) return <h1>Loading...</h1>;
+
+  if (isError) return <h1>Error...</h1>;
+
+  return (
+    <section className='gallery'>
+      <h1>Gallery</h1>
+      {data.data.results.map((image) => {
+        const { id, urls, alt_description } = image;
+        return (
+          <article key={id} className='image'>
+            <img src={urls.regular} alt={alt_description} />
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 export default Gallery;
